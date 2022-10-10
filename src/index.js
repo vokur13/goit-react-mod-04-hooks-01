296bf38d5025d4d4acce10bc3a63c529a21fb10c
vokur13 <vokur13@gmail.com>
2022-10-10 19:29:27 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from './App/App';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import AuthProvider from './contexts/auth/Provider';
+import 'modern-normalize/modern-normalize.css';
 import './index.css';
-import { ThemeProvider } from 'styled-components';
-import { theme } from './theme';
+import 'react-toastify/dist/ReactToastify.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
